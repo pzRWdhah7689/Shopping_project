35dd5fda9ae09b2d2b607759e6d2d588e0939f8a
Aman <amanghouri614@gmail.com>
2024-07-05 16:41:33 +0500

    final commit

---
+++ app/src/main/java/com/example/shopping_project/DisplayProductsActivity.java
@@ -0,0 +1,4 @@
+package com.example.shopping_project;
+
+public class DisplayProductsActivity {
+}
---
+++ app/src/main/java/com/example/shopping_project/OnRecyclerViewClickListener.java
@@ -0,0 +1,5 @@
+package com.example.shopping_project;
+
+public interface OnRecyclerViewClickListener {
+    void OnItemClick(int productId);
+}
